9930356adf51868c4101d6b5493f1fb4ce336ca1
Joshua Gourgey <43460117+Gourgey@users.noreply.github.com>
2026-05-16 14:05:02 +0100

    chrome cached asset updated

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,8 +6,14 @@ function readJson(relativePath) {
   return JSON.parse(fs.readFileSync(filePath, "utf8"));
 }
 
+function fileVersion(relativePath) {
+  const filePath = path.join(__dirname, "..", "..", relativePath);
+  return Math.round(fs.statSync(filePath).mtimeMs).toString(36);
+}
+
 module.exports = {
   settings: readJson("settings.json"),
+  cssVersion: fileVersion("assets/css/styles.css"),
   home: readJson("pages/home.json"),
   about: readJson("pages/about.json"),
   contact: readJson("pages/contact.json"),
